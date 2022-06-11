bc3d3a0ff749f79b733b27f2ed5b1855712ab432
janKozie1 <jankkoziel@gmail.com>
2022-06-11 23:04:23 +0200

    custom build process

---
+++ postBuild.sh
@@ -0,0 +1,3 @@
+mkdir ./pantryy_backend/dist/static
+cp ./pantryy_frontend/public/index.html ./pantryy_backend/dist/static/index.html
+cp -r ./pantryy_frontend/public/static/. ./pantryy_backend/dist/static/
